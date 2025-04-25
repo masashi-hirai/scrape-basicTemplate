af5cba42ab6559c70da5f2b91747824873591c8b
masashi <haramasashi123@gmail.com>
2025-04-25 09:03:28 +0900

    run_basicTemplate.pyを動かせる状態に

--- projects/basicTemlate_scrape/basicTemlate_scrape/spiders/googlemap_basic.py
+++ projects/basicTemlate_scrape/basicTemlate_scrape/spiders/googlemap_basic.py
@@ -3,8 +3,8 @@ import scrapy
 
 class GooglemapBasicSpider(scrapy.Spider):
     name = 'googlemap_basic'
-    allowed_domains = ['www.google.co.jp/maps']
-    start_urls = ['http://www.google.co.jp/maps/']
+    allowed_domains = ['google.co.jp']
+    start_urls = ['https://www.google.co.jp/maps/?hl=ja']
 
     def parse(self, response):
         pass
